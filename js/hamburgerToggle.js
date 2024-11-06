f7f9af0787fe924844c555b79fd9270caf6762fd
ManasTom <manastom670@gmail.com>
2024-11-06 16:45:49 +0530

    home1/2

---
+++ js/hamburgerToggle.js
@@ -0,0 +1,8 @@
+function hamburgerToggle() {
+    var menu = document.getElementById("mobileMenu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
